71ee81c08d3cf67056f5eee1a30f93b40486c4f6
Rafael Pioli <rspioli.rf@gmail.com>
2021-09-10 06:29:54 -0300

    Alterado o nome do controller de teste para encomenda

---
+++ RastreiaEncomenda.Web/Controllers/EncomendaController.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RastreiaEncomenda.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EncomendaController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> ObtemInformacao()
+        {
+            return Ok(new {
+                Codigo = "ON4676363876BR"                
+            });
+        }
+    }
+}
