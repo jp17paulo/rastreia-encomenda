53c57c62c414058910af8b4a9d45d7ffcf9a6fef
jp17paulo <jp17paulo@terra.com.br>
2021-09-19 16:56:08 -0300

    Acessando o site dos correios utilizando o selenium, passando o código a ser pesquisado, consultando e retornando os últimos dados da encomenda

--- RastreiaEncomenda.Web/Controllers/EncomendaController.cs
+++ RastreiaEncomenda.Web/Controllers/EncomendaController.cs
@@ -1,5 +1,10 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using HtmlAgilityPack;
 
 namespace RastreiaEncomenda.Web.Controllers
 {
@@ -9,17 +14,67 @@ namespace RastreiaEncomenda.Web.Controllers
     {
         [HttpGet]
         [Route("api/[controller]")]
-                                                //api/encomenda?codigo=OP862398045B&sro="Objeto entregue ao destinatário"
-        public async Task<IActionResult> ObtemInformacao(string codigo, string sro)
+                                         //api/encomenda?codigo=OP862398045BR
+        public async Task<IActionResult> ObtemInformacao(string codigo)
         {
-            await Task.Delay(1);
-            return Ok(new {
-                Codigo = codigo,
-                Sro = sro
+            string url = "https://www2.correios.com.br/sistemas/rastreamento/default.cfm";
 
+            ChromeDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(url);
+            //driver.Url = url;
+            //driver.Manage().Window.Maximize();
+
+            await Task.Delay(1);
+
+            var pesquisaEl = driver.FindElement(By.Id("objetos"));
+            pesquisaEl.SendKeys(codigo);
+
+            var buscarEl = driver.FindElement(By.Id("btnPesq"));
+            buscarEl.Click();
+
+
+
+            var dataCidadeAtual = driver.FindElement(By.ClassName("sroDtEvent")).Text;
+            var status          =  driver.FindElement(By.ClassName("sroLbEvent")).Text;
+
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+
+            driver.Close();
+
+            await Task.Delay(1);
+
+            //TODO
+            //foreach (HtmlNode node in htmlDocument.GetElementbyId("ver-rastro-unico").ChildNodes)
+            //{
+            //    if (node.Attributes.Count > 0)
+            //    {
+            //        List<data> _data = new List<data>();
+            //        _data.Add(new data()
+            //        {
+            //            DataPostagem = node.Attributes["step"].Value,
+
+            //        });
+
+            //        json = JsonConvert.SerializeObject(_data.ToArray());
+            //    }
+
+            //}
+
+            return Ok(new
+            {
+                DataPostagem = dataCidadeAtual,
+                Status = status
             });
+
         }
 
-       
+        public class Data
+        {
+            public string DataPostagem { get; set; }
+            public string StatusEntrega { get; set; }
+        }
+
     }
 }
