ae19295dee46b6898378733021ad8f846f2c8a2e
jp17paulo <jp17paulo@terra.com.br>
2021-10-08 19:32:04 -0300

    Criado arquivo json com a lista retornada de eventos passando para o front-end, criadas as lógicas tanto da classe de serviço Playwright quanto a do Selenium

--- RastreiaEncomenda.Web/Controllers/EncomendaController.cs
+++ RastreiaEncomenda.Web/Controllers/EncomendaController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RastreiaEncomenda.Web.Servicos.Interfaces;
 using Microsoft.Playwright;
+using System.Collections.Generic;
 
 namespace RastreiaEncomenda.Web.Controllers
 {
@@ -26,50 +27,14 @@ namespace RastreiaEncomenda.Web.Controllers
             return Ok(resultado);
         }
 
-        //api/encomenda?codigo=OP862398045BR
+       
         [HttpGet]
         [Route("api/[controller]")]
         public async Task<IActionResult> ObtemInformacao(string codigo)
         {
-            string lista = "";
-
-            using var playwright = await Playwright.CreateAsync();
-            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false,
-            });
-
-            var context = await browser.NewContextAsync();
-
-            // Open new page
-            var page = await context.NewPageAsync();
-
-            // Go to https://www2.correios.com.br/sistemas/rastreamento/default.cfm
-            await page.GotoAsync("https://www2.correios.com.br/sistemas/rastreamento/default.cfm");
-
-            // Click textarea[name="objetos"]
-            await page.ClickAsync("textarea[name=\"objetos\"]");
-
-            // Fill textarea[name="objetos"]
-            await page.FillAsync("textarea[name=\"objetos\"]", "OP862398045BR");
-
-            // Click text=Buscar
-            await page.ClickAsync("text=Buscar");
-
-           var tabela = await page.QuerySelectorAllAsync("table.listEvent");
-
-            foreach(var linha in tabela)
-            {
-                var tabelaDataDoEvento = await linha.QuerySelectorAsync("td.sroDtEvent");
-                var dataEvento = tabelaDataDoEvento.InnerTextAsync();
-                lista += dataEvento.Result.Replace("\n"," ") + " ";
-
-                var tabelaDescricao = await linha.QuerySelectorAsync("td.sroLbEvent");
-                var descricao = await tabelaDescricao.InnerTextAsync();
-                lista += descricao.Replace("\n", " ") + " ";
-            }
-
-            return Ok(lista);
+                                            //api/encomenda?codigo=OP862398045BR
+            var resultado = await _encomendaServico.ObtemStatusEncomenda(codigo);
+            return Ok(resultado);
 
         }
 
--- RastreiaEncomenda.Web/Servicos/EncomendaServicoSelenium.cs
+++ RastreiaEncomenda.Web/Servicos/EncomendaServicoSelenium.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using RastreiaEncomenda.Web.Servicos.Interfaces;
 using RastreiaEncomenda.Web.ViewModels;
 
@@ -8,9 +11,38 @@ namespace RastreiaEncomenda.Web.Servicos
     {
         public async Task<ConsultaStatusEncomendaViewModel> ObtemStatusEncomenda(string codigo)
         {
-            // aqui vai a lógica usando o Selenium
-            await Task.Delay(1);
-            return new ConsultaStatusEncomendaViewModel("ON77334347", "Entregue via Selenium");
+            //aqui vai a lógica usando o Selenium
+            var list = new List<dynamic>();
+
+            string url = "https://www2.correios.com.br/sistemas/rastreamento/default.cfm";
+
+            ChromeDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(url);
+            //driver.Url = url;
+            //driver.Manage().Window.Maximize();
+
+            var pesquisaEl =  driver.FindElement(By.Id("objetos"));
+            pesquisaEl.SendKeys(codigo);
+
+            var buscarEl = driver.FindElement(By.Id("btnPesq"));
+            buscarEl.Click();
+
+            var datasCidadesAtuais = driver.FindElements(By.ClassName("sroDtEvent"));
+            var status = driver.FindElements(By.ClassName("sroLbEvent"));
+            var i = datasCidadesAtuais.Count;
+
+            foreach (var node in datasCidadesAtuais)
+            {
+                list.Add(new {
+                    Data = node.Text.Replace("\r", " ").Replace("\n", " ") + " ",
+                    Status = status[i].Text.Replace("\r", " ").Replace("\n", " ") + " "
+                });
+                i++;
+            }
+
+
+            return new ConsultaStatusEncomendaViewModel(codigo, list);
         }
     }
 }
